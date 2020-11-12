6b4bfecf0ea6de264d5572581a7534712f8ec789
KolumbanA <attila99kolumban@gmail.com>
2020-11-12 14:31:52 +0200

    2d_movement1.0(crouching, better hitboxes, jumping, running)

--- fox_shooter/Assets/PlayerMovement.cs
+++ fox_shooter/Assets/PlayerMovement.cs
@@ -11,21 +11,35 @@ public class PlayerMovement : MonoBehaviour
 	public float runSpeed = 40f;
 	float horizontalMove = 0f;
 	bool jump=false;
+	bool crouch=false;
 
 
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-	if(Input.GetButtonDown("Jump"))
-	{
-		jump=true;
-	}
+		if(Input.GetButtonDown("Jump"))
+		{
+			jump=true;
+		}
+		
+		if(Input.GetButtonDown("Crouch"))
+		{
+			crouch=true;
+		}else if(Input.GetButtonUp("Crouch"))
+		{
+			crouch = false;
+		}
+	
+	
+	
     }
 	
 	void FixedUpdate()
 	{
-		controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
 		jump = false;
+		
+		
 	}
 }
