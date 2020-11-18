d14b818f9f6d984c5eed00f59ef7ea418f75f42f
KolumbanA <attila99kolumban@gmail.com>
2020-11-18 23:37:44 +0200

    player_animation
    Animating player movement(running/jumping/crouching)

--- fox_shooter/Assets/PlayerMovement.cs
+++ fox_shooter/Assets/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
 	
 	public CharacterController2D controller;
+	public Animator animator;
 
 
 	public float runSpeed = 40f;
@@ -18,9 +19,13 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+		
+		animator.SetFloat("player_speed", Mathf.Abs(horizontalMove));
+
 		if(Input.GetButtonDown("Jump"))
 		{
 			jump=true;
+			animator.SetBool("player_is_jumping",true);
 		}
 		
 		if(Input.GetButtonDown("Crouch"))
@@ -34,6 +39,15 @@ public class PlayerMovement : MonoBehaviour
 	
 	
     }
+	
+	public void OnLanding(){
+		animator.SetBool("player_is_jumping",false);
+	}
+	
+	public void onCrouching( bool isCrouching)
+	{
+		animator.SetBool("player_is_crouching",isCrouching);
+	}
 	
 	void FixedUpdate()
 	{
