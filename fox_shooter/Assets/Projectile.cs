f35ab717ea32dfb1851d9323d49b5b37216bccb0
KolumbanA <attila99kolumban@gmail.com>
2020-12-10 18:37:26 +0200

    Player shooting
    Player shooting functional, needs some more work to look good

---
+++ fox_shooter/Assets/Projectile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+    public float distance;
+    public LayerMask whatIsSolid;
+    public GameObject destroyEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up);
+
+        if(hitInfo.collider != null)
+        {
+            if (hitInfo.collider.CompareTag("Enemy"))
+            {
+                Debug.Log("ENEMY HIT");
+            }
+            //DestroyProjectile();
+        }
+
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    void DestroyProjectile()
+    {
+        //Instantiate(destroyEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
