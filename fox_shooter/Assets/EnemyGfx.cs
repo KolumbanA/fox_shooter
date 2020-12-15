1589304a20e3b04b284121d8be3cd1ded551ed7a
KolumbanA <attila99kolumban@gmail.com>
2020-12-15 16:14:08 +0200

    Enemy ai pathfinding
    Implementing pathfinding for the flying enemy

---
+++ fox_shooter/Assets/EnemyGfx.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyGfx : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public AIPath aiPath;
+	
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(aiPath.desiredVelocity.x >= 0.01f)
+		{
+			transform.localScale = new Vector3(-4f, 4f, 4f);
+		}
+		else if(aiPath.desiredVelocity.x <= -0.01f)
+		{
+			transform.localScale = new Vector3(4f, 4f, 4f);
+		}
+    }
+}
